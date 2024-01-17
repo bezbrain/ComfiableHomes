85cf805be9c996af2db697b97198589017d8dd46
bezbrain <belloquadriolawale@gmail.com>
2024-01-17 21:24:42 +0100

    Refactor the delivery info

--- src/components/routes/checkout/deliveryInfo.jsx
+++ src/components/routes/checkout/deliveryInfo.jsx
@@ -6,7 +6,7 @@ import { useCheckoutContext } from "../../../contexts/checkoutContext";
 const DeliveryInfo = () => {
   const { editController } = useCheckoutContext();
   return (
-    <form className="checkout-form">
+    <section className="checkout-form">
       <div className="checkout-header">
         <h2>Delivery Information</h2>
         {editController && (
@@ -15,23 +15,25 @@ const DeliveryInfo = () => {
           </p>
         )}
       </div>
-      <div className="names-con checkout-input-con">
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-      </div>
-      <div className="address-con checkout-input-con">
-        <input type="text" placeholder="Address" />
-      </div>
-      <div className="location-con checkout-input-con">
-        <input type="text" placeholder="City/Town" />
-        <input type="text" placeholder="Zip Code" />
-      </div>
-      <div className="contact-con checkout-input-con">
-        <input type="text" placeholder="Mobile Number" />
-        <input type="text" placeholder="Email Address" />
-      </div>
-      <button className="checkout-info-btn">Submit Address</button>
-    </form>
+      <form>
+        <div className="names-con checkout-input-con">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <div className="address-con checkout-input-con">
+          <input type="text" placeholder="Address" />
+        </div>
+        <div className="location-con checkout-input-con">
+          <input type="text" placeholder="City/Town" />
+          <input type="text" placeholder="Zip Code" />
+        </div>
+        <div className="contact-con checkout-input-con">
+          <input type="text" placeholder="Mobile Number" />
+          <input type="text" placeholder="Email Address" />
+        </div>
+        <button className="checkout-info-btn">Submit Address</button>
+      </form>
+    </section>
   );
 };
 
