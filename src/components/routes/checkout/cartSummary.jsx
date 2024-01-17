7c04df2e6e9a0acdae38d2f574229257bffdf22b
bezbrain <belloquadriolawale@gmail.com>
2024-01-17 21:16:53 +0100

    Design the pay now section

--- src/components/routes/checkout/cartSummary.jsx
+++ src/components/routes/checkout/cartSummary.jsx
@@ -4,12 +4,25 @@ import "../../../styles/checkout/cartSummary.css";
 const CartSummary = () => {
   return (
     <div className="checkout-cart-summary">
-      <h2>CART SUMMARY</h2>
-      <div>
-        <p>Subtotal</p>
+      <h3>Order Summary</h3>
+      <hr />
+      <div className="items-total">
+        <p>
+          Item's total <span>(2)</span>
+        </p>
         <p>N 2,525</p>
       </div>
-      <button>PAY NOW</button>
+      <hr />
+      <div className="checkout-items-total">
+        <h4>Total</h4>
+        <h2>N2,525</h2>
+      </div>
+      <hr />
+      <button className="checkout-pay-now-btn">PAY NOW</button>
+      <p className="checkout-condition">
+        By proceeding, you are automatically accepting the{" "}
+        <a href="">Terms & Conditions</a>
+      </p>
     </div>
   );
 };
