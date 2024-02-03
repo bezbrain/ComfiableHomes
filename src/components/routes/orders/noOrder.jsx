e42e9cb45a589b722e4f75652d14a58b52a3c8c6
bezbrain <belloquadriolawale@gmail.com>
2024-02-03 18:43:43 +0100

    Reduce the no-order text

--- src/components/routes/orders/noOrder.jsx
+++ src/components/routes/orders/noOrder.jsx
@@ -8,7 +8,7 @@ const NoOrder = ({ noOrder }) => {
         src="https://th.bing.com/th/id/OIP.XVKrnIzpHLbIF-jRUqiy1gHaFj?w=273&h=205&c=7&r=0&o=5&pid=1.7"
         alt=""
       />
-      <p>{noOrder}</p>
+      <p>{noOrder}!</p>
     </div>
   );
 };
